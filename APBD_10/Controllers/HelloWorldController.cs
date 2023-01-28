687dda1be031d9ea1c6f2e0366f9af431cbfc783
Mateusz Filipek <s15348@pjwstk.edu.pl>
2023-01-28 11:42:10 +0100

    Zadanie10

---
+++ APBD_10/Controllers/HelloWorldController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace APBD_10.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // 
+        // GET: /HelloWorld/
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+
+            return View();
+        }
+    }
+}
---
+++ APBD_10/Data/APBD_10Context.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace APBD_10.Data
+{
+    public class APBD_10Context : DbContext
+    {
+        public APBD_10Context(DbContextOptions<APBD_10Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<APBD_10.Models.Movie> Movie { get; set; }
+    }
+}
